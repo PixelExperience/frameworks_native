1ce707d72cba427b4d0afb109bc7fb4aeeec6cbe
Naseer Ahmed <naseer@codeaurora.org>
2017-11-09 15:38:25 -0500

    SF: Add support to disable animation on external
    CRs-Fixed: 2141089
    Change-Id: I7ca4c9302222dcd557b147499637a221d6e57a7d

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -476,6 +476,9 @@ private:
     virtual void handleDPTransactionIfNeeded(
                      const Vector<DisplayState>& /*displays*/) { }
 
+    virtual void setDisplayAnimating(const sp<const DisplayDevice>& /*hw*/,
+                                     const int32_t& /*dpy*/) { }
+
     /* ------------------------------------------------------------------------
      * Message handling
      */
