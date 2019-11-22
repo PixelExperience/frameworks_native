d73d460857518904c63cfba0e2305f29678eb33b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-22 11:58:37 +0000

    surfaceflinger: Merge caf changes
    * From LA.UM.8.1.r1-10800-sm8150.0
    
    Change-Id: I787ef516e5fbe71af6a2cb14dfbe9cdebefc31ca

--- services/surfaceflinger/BufferLayer.cpp
+++ services/surfaceflinger/BufferLayer.cpp
@@ -66,6 +66,8 @@ BufferLayer::BufferLayer(const LayerCreationArgs& args)
 
     mPotentialCursor = args.flags & ISurfaceComposerClient::eCursorWindow;
     mProtectedByApp = args.flags & ISurfaceComposerClient::eProtectedByApp;
+
+    mScreenshot = (std::string(args.name).find("ScreenshotSurface") != std::string::npos);
 }
 
 BufferLayer::~BufferLayer() {
@@ -587,6 +589,7 @@ bool BufferLayer::latchUnsignaledBuffers() {
         latch = atoi(value);
         propertyLoaded = true;
     }
+
     return latch;
 }
 
