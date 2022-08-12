24cea3d0a76f96fe92450d7e6c93c7c9bb33f5da
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-12 23:25:57 +0530

    SF: Allow forcing HWC brightness support
    Some vendors purposely disable brightness capability
    even though the HWC completely supports it, in order
    to leverage their custom brightness implementations.
    One such example of this is in stock HWC of some
    xiaomi devices.
    
    Since light HAL offers lesser granual control than
    HWC (since its in gamma space and not float), and
    since it would be rather difficult to patch the blob
    to return the right capabilities, allow forcing the
    usage of HWC brightness functions via a prop:
    
    ro.sf.force_hwc_brightness=1 (default 0).
    
    Change-Id: I7d2fef03afbbb936572c717f769f30c9e49213e0
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1373,6 +1373,7 @@ private:
 
     bool mLumaSampling = true;
     bool mForceLightBrightness = false;
+    bool mForceHwcBrightness = false;
     sp<RegionSamplingThread> mRegionSamplingThread;
     sp<FpsReporter> mFpsReporter;
     sp<TunnelModeEnabledReporter> mTunnelModeEnabledReporter;
