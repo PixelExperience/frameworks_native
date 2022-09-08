86dc0166a0f632270c0029d0d7d84ca718c47b81
Josh Gao <jmgao@fb.com>
2022-09-08 16:19:29 -0700

    SurfaceFlinger: add more thread-safety annotations.
    As part of debugging a crash in SurfaceFlinger, we became suspicious of
    markLayerPendingRemovalLocked because it wasn't annotated with thread
    safety annotations. This ended up being a red herring, because
    mStateLock is held on all paths into that function. Add some helpers to
    MutexUtils.h to require/assert mutexes, and use them to sprinkle some
    thread safety annotations to make things more clear.
    
    Test: m surfaceflinger
    Change-Id: Ib48433765bb6ffcb351ec10cd5bc89e254b48545

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -4423,6 +4423,7 @@ uint32_t SurfaceFlinger::setClientStateLocked(const FrameTimelineInfo& frameTime
         }
         return 0;
     }
+    MUTEX_ALIAS(mStateLock, layer->mFlinger->mStateLock);
 
     // Only set by BLAST adapter layers
     if (what & layer_state_t::eProducerDisconnect) {
@@ -7341,6 +7342,7 @@ void SurfaceFlinger::handleLayerCreatedLocked(const LayerCreatedState& state) {
         ALOGD("Layer was destroyed soon after creation %p", state.layer.unsafe_get());
         return;
     }
+    MUTEX_ALIAS(mStateLock, layer->mFlinger->mStateLock);
 
     sp<Layer> parent;
     bool addToRoot = state.addToRoot;
